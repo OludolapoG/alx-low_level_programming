48ca95d6d086c9d6e6165f36988378a03bad3bc6
Oludolapo Gbadebo <dolarpy@gmail.com>
2022-06-30 13:37:34 -0700

    C project advanced tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	long long int lli;
 	float f;
 
-	printf("Size of a char: %zu byte(s)\n", sizeof(char c));
-	printf("Size of an int: %zu byte(s)\n", sizeof(int i));
-	printf("Size of a long int: %zu byte(s)\n", sizeof(long int li));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int lli));
-	printf("Size of a float: %zu byte(s)\n", sizeof(float f));
+	printf("Size of a char: %zu byte(s)\n", sizeof(c));
+	printf("Size of an int: %zu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(lli));
+	printf("Size of a float: %zu byte(s)\n", sizeof(f));
 	return (0);
 }
